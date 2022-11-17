d59cdfb89903420d09c6445549fac4781c313a95
윤소현 <wja06018@naver.com>
2022-11-18 01:01:26 +0900

    Init

---
+++ whereismyhome_final/src/main/java/com/ssafy/home/WhereismyhomeFinalApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhereismyhomeFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhereismyhomeFinalApplication.class, args);
+	}
+
+}
