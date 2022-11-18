e4405fb0971f0d6486afe4e450354f4e7e0b4930
박성환 <cyctjdghks@naver.com>
2022-11-18 11:36:10 +0900

    init: init project

---
+++ whereismyhome_final/src/main/java/com/ssafy/home/WhereismyhomeFinalApplication.java
@@ -0,0 +1,14 @@
+package com.ssafy.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhereismyhomeFinalApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+		SpringApplication.run(WhereismyhomeFinalApplication.class, args);
+	}
+
+}
