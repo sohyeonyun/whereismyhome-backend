47504e45072afe2bfbff8c01d8791030ed81db6e
박성환 <cyctjdghks@naver.com>
2022-11-19 17:38:21 +0900

    update: update swagger

--- whereismyhome_final/src/main/java/com/ssafy/home/user/model/service/UserServiceImpl.java
+++ whereismyhome_final/src/main/java/com/ssafy/home/user/model/service/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.ssafy.home.user.model.service;
 
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,6 +14,7 @@ public class UserServiceImpl implements UserService {
 
 	private UserMapper mapper = null;
 
+	@Autowired
 	private UserServiceImpl(UserMapper mapper) {
 		this.mapper = mapper;
 	}
