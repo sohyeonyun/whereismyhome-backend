c5d70ed49341123eb810467cc238c2a8b7ba31d4
박성환 <cyctjdghks@naver.com>
2022-11-22 00:35:09 +0900

    feat: feature map 3

--- whereismyhome_final/src/main/java/com/ssafy/home/controller/MapController.java
+++ whereismyhome_final/src/main/java/com/ssafy/home/controller/MapController.java
@@ -11,11 +11,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.home.model.MapDTO;
 import com.ssafy.home.model.MapLocationDTO;
+import com.ssafy.home.model.MapSearchOptionDTO;
 import com.ssafy.home.model.service.MapService;
 
 import io.swagger.annotations.Api;
@@ -41,7 +44,7 @@ public class MapController {
 			@PathVariable("name") @ApiParam(value = "시 구군 동 검색 이름.", required = true) String name) {
 		logger.info("mapLocationList - 호출");
 		logger.info("mapLocationList name : {}", name);
-		
+
 		try {
 			ArrayList<MapLocationDTO> list = mapService.listMapLocation(name);
 			logger.info("mapLocationList list : {} ", list);
@@ -53,15 +56,26 @@ public class MapController {
 	}
 
 //	listMapDong
-	@GetMapping("/dong/{dongcode}")
+	@PostMapping("/dong/{dongcode}")
 	@ApiOperation(value = "지역 선택 시 매매 목록", response = ArrayList.class)
-	public ResponseEntity<?> mapDongList(@PathVariable("dongcode") @ApiParam(value = "선택한 지역.", required = true) String dongcode) {
+	public ResponseEntity<?> mapDongList(
+			@PathVariable("dongcode") @ApiParam(value = "선택한 지역.", required = true) String dongCode,
+			@RequestBody @ApiParam(value = "추가 옵션(최소 거래금액, 최대 거래금액, 최소 크기, 최대 크기, 거래일)", required = true) MapSearchOptionDTO dto) {
 		logger.info("mapDongList - 호출");
+		logger.info("mapDongList dongCode : {}", dongCode);
+		logger.info("mapDongList dto : {}", dto);
 
+		Map<String, Object> map = new HashMap<>();
+		map.put("dongCode", dongCode);
+		map.put("lowDealAmount", dto.getLowDealAmount());
+		map.put("highDealAmount", dto.getHighDealAmount());
+		map.put("lowArea", dto.getLowArea());
+		map.put("highArea", dto.getHighArea());
+		map.put("year", dto.getYear());
+		
 		try {
-			Map<String, String> map = new HashMap<>();
 			ArrayList<MapDTO> list = mapService.listMapDong(map);
-			logger.info("mapDongList list : {} ", list);
+			logger.info("mapDongList map : {} ", map);
 			return new ResponseEntity<ArrayList>(list, HttpStatus.OK);
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -70,15 +84,26 @@ public class MapController {
 	}
 
 //	listMapApt
-	@GetMapping("/apt/{aptCode}")
+	@PostMapping("/apt/{apartMentName}")
 	@ApiOperation(value = "아파트 선택 시 매매 목록", response = ArrayList.class)
-	public ResponseEntity<?> mapAptList(@PathVariable("aptCode") @ApiParam(value = "선택한 아파트.", required = true) String aptCode) {
+	public ResponseEntity<?> mapAptList(
+			@PathVariable("apartMentName") @ApiParam(value = "선택한 아파트.", required = true) String apartMentName,
+			@RequestBody @ApiParam(value = "추가 옵션(최소 거래금액, 최대 거래금액, 최소 크기, 최대 크기, 거래일)", required = true) MapSearchOptionDTO dto) {
 		logger.info("mapAptList - 호출");
-
+		logger.info("mapAptList apartMentName : {}", apartMentName);
+		logger.info("mapAptList dto : {}", dto);
+		
+		Map<String, Object> map = new HashMap<>();
+		map.put("apartMentName", apartMentName);
+		map.put("lowDealAmount", dto.getLowDealAmount());
+		map.put("highDealAmount", dto.getHighDealAmount());
+		map.put("lowArea", dto.getLowArea());
+		map.put("highArea", dto.getHighArea());
+		map.put("year", dto.getYear());
+		
 		try {
-			Map<String, String> map = new HashMap<>();
 			ArrayList<MapDTO> list = mapService.listMapApt(map);
-			logger.info("mapAptList list : {} ", list);
+			logger.info("mapAptList map : {} ", map);
 			return new ResponseEntity<ArrayList>(list, HttpStatus.OK);
 		} catch (Exception e) {
 			e.printStackTrace();
--- whereismyhome_final/src/main/java/com/ssafy/home/model/service/MapService.java
+++ whereismyhome_final/src/main/java/com/ssafy/home/model/service/MapService.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import com.ssafy.home.model.MapDTO;
 import com.ssafy.home.model.MapLocationDTO;
+import com.ssafy.home.model.MapSearchOptionDTO;
 
 public interface MapService {
 	// 맵
@@ -12,8 +13,10 @@ public interface MapService {
 	ArrayList<MapLocationDTO> listMapLocation(String name) throws Exception;
 
 	// 동코드로 검색
-	ArrayList<MapDTO> listMapDong(Map<String, String> map) throws Exception;
+	ArrayList<MapDTO> listMapDong(Map<String, Object> map) throws Exception;
+//	ArrayList<MapDTO> listMapDong(String dongCode, MapSearchOptionDTO dto) throws Exception;
 
 	// 아파트 이름으로 검색
-	ArrayList<MapDTO> listMapApt(Map<String, String> map) throws Exception;
+	ArrayList<MapDTO> listMapApt(Map<String, Object> map) throws Exception;
+//	ArrayList<MapDTO> listMapApt(String apartMentName, MapSearchOptionDTO dto) throws Exception;
 }
