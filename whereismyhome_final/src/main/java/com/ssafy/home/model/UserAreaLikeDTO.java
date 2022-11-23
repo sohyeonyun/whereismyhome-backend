5268de303a2eeadd09f85110cf988e2d0bf919af
박성환 <cyctjdghks@naver.com>
2022-11-23 22:10:31 +0900

    feat: update sql

--- whereismyhome_final/src/main/java/com/ssafy/home/model/UserAreaLikeDTO.java
+++ whereismyhome_final/src/main/java/com/ssafy/home/model/UserAreaLikeDTO.java
@@ -6,39 +6,40 @@ import io.swagger.annotations.ApiModelProperty;
 @ApiModel(value = "UserAreaLikeDTO : 유저 관심 지역 정보", description = "유저의 관심 지역 정보를 나타낸다.")
 public class UserAreaLikeDTO {
 
-	@ApiModelProperty(value = "시도 이름")
-	private String sidoName;
-	@ApiModelProperty(value = "구군 이름")
-	private String gugunName;
-	@ApiModelProperty(value = "동 이름")
-	private String dongName;
+	@ApiModelProperty(value = "지역 이름")
+	private String area;
+	@ApiModelProperty(value = "등록 시간")
+	private String registerTime;
+	@ApiModelProperty(value = "동 코드")
+	private String dongcode;
 
-	public String getSidoName() {
-		return sidoName;
+	public String getArea() {
+		return area;
 	}
 
-	public void setSidoName(String sidoName) {
-		this.sidoName = sidoName;
+	public void setArea(String area) {
+		this.area = area;
 	}
 
-	public String getGugunName() {
-		return gugunName;
+	public String getRegisterTime() {
+		return registerTime;
 	}
 
-	public void setGugunName(String gugunName) {
-		this.gugunName = gugunName;
+	public void setRegisterTime(String registerTime) {
+		this.registerTime = registerTime;
 	}
 
-	public String getDongName() {
-		return dongName;
+	public String getDongcode() {
+		return dongcode;
 	}
 
-	public void setDongName(String dongName) {
-		this.dongName = dongName;
+	public void setDongcode(String dongcode) {
+		this.dongcode = dongcode;
 	}
 
 	@Override
 	public String toString() {
-		return "UserAreaLikeDTO [sidoName=" + sidoName + ", gugunName=" + gugunName + ", dongName=" + dongName + "]";
+		return "UserAreaLikeDTO [area=" + area + ", registerTime=" + registerTime + ", dongcode=" + dongcode + "]";
 	}
+
 }
--- whereismyhome_final/src/main/java/com/ssafy/home/model/UserHouseLikeDTO.java
+++ whereismyhome_final/src/main/java/com/ssafy/home/model/UserHouseLikeDTO.java
@@ -8,24 +8,10 @@ public class UserHouseLikeDTO {
 
 	@ApiModelProperty(value = "아파트 이름")
 	private String apartmentName;
-	@ApiModelProperty(value = "아파트 위치")
-	private String location;
-	@ApiModelProperty(value = "아파트 건축연도")
-	private String buildYear;
-	@ApiModelProperty(value = "아파트 거래 가격")
-	private String dealAmount;
-	@ApiModelProperty(value = "아파트 거래 날짜")
-	private String date;
-	@ApiModelProperty(value = "아파트 평수")
-	private String area;
-	@ApiModelProperty(value = "아파트 층수")
-	private String floor;
-	@ApiModelProperty(value = "아파트 위도")
-	private String lat;
-	@ApiModelProperty(value = "아파트 경도")
-	private String lng;
-	@ApiModelProperty(value = "아파트 좋아요 눌린 수")
-	private String house_like_count;
+	@ApiModelProperty(value = "등록 시간")
+	private String registerTime;
+	@ApiModelProperty(value = "아파트 코드")
+	private String aptcode;
 
 	public String getApartmentName() {
 		return apartmentName;
@@ -35,83 +21,26 @@ public class UserHouseLikeDTO {
 		this.apartmentName = apartmentName;
 	}
 
-	public String getLocation() {
-		return location;
+	public String getRegisterTime() {
+		return registerTime;
 	}
 
-	public void setLocation(String location) {
-		this.location = location;
+	public void setRegisterTime(String registerTime) {
+		this.registerTime = registerTime;
 	}
 
-	public String getBuildYear() {
-		return buildYear;
+	public String getAptcode() {
+		return aptcode;
 	}
 
-	public void setBuildYear(String buildYear) {
-		this.buildYear = buildYear;
-	}
-
-	public String getDealAmount() {
-		return dealAmount;
-	}
-
-	public void setDealAmount(String dealAmount) {
-		this.dealAmount = dealAmount;
-	}
-
-	public String getDate() {
-		return date;
-	}
-
-	public void setDate(String date) {
-		this.date = date;
-	}
-
-	public String getArea() {
-		return area;
-	}
-
-	public void setArea(String area) {
-		this.area = area;
-	}
-
-	public String getFloor() {
-		return floor;
-	}
-
-	public void setFloor(String floor) {
-		this.floor = floor;
-	}
-
-	public String getLat() {
-		return lat;
-	}
-
-	public void setLat(String lat) {
-		this.lat = lat;
-	}
-
-	public String getLng() {
-		return lng;
-	}
-
-	public void setLng(String lng) {
-		this.lng = lng;
-	}
-
-	public String getHouse_like_count() {
-		return house_like_count;
-	}
-
-	public void setHouse_like_count(String house_like_count) {
-		this.house_like_count = house_like_count;
+	public void setAptcode(String aptcode) {
+		this.aptcode = aptcode;
 	}
 
 	@Override
 	public String toString() {
-		return "UserHouseLikeDTO [apartmentName=" + apartmentName + ", location=" + location + ", buildYear="
-				+ buildYear + ", dealAmount=" + dealAmount + ", date=" + date + ", area=" + area + ", floor=" + floor
-				+ ", lat=" + lat + ", lng=" + lng + ", house_like_count=" + house_like_count + "]";
+		return "UserHouseLikeDTO [apartmentName=" + apartmentName + ", registerTime=" + registerTime + ", aptcode="
+				+ aptcode + "]";
 	}
 
 }
